5233c3101705d0b4015de335c0056fba4eb91091
Jimi331492 <jimi331492@gmail.com>
2022-04-20 10:58:25 +0800

    完成回复评论以及评论图片

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //app.js
+const http = require('./utils/httputils.js');
+const ContentTypeEnum = require('./utils/ContentTypeEnum.js');
 App({
     require: function ($uri) {
         return wx.require($uri)
@@ -65,6 +67,86 @@ App({
 
                 }
             }
+        })
+    },
+
+    //登录
+    getUserProfile: function () {
+        wx.login({
+            success: res => {
+                if (res.code) {
+                    wx.setStorageSync('code', res.code);
+                }
+            }
+        })
+
+        wx.getUserProfile({
+            desc: '用于完善资料',
+            success: res => {
+
+                const form = {
+                    signature: res.signature,
+                    rawData: res.rawData,
+                    encryptedData: res.encryptedData,
+                    iv: res.iv,
+                    jscode: wx.getStorageSync('code'),
+                    appid: "wxeb4f620b577ff31a"
+                }
+
+                http.postRequest("/mp_get_unionId", form, ContentTypeEnum.Json_Sub,
+                    res => {
+
+                        wx.setStorageSync('sessionId', res.data.sessionId);
+                        wx.setStorageSync('unionId', res.data.unionId);
+                        wx.setStorageSync('openid', res.data.openId);
+                        http.fill_token_toheader(res.data.unionId);
+                        wx.setStorageSync('isLogin', true)
+                        http.postRequest("/getMPUserInfo", res.data.unionId, ContentTypeEnum.Default_Sub,
+                            res => {
+                                wx.setStorageSync('userInfo', res.data);
+                                wx.redirectTo({
+                                    url: '/pages/index/home/home',
+                                })
+                            },
+                            err => {
+                                wx.showToast({
+                                    title: err.message,
+                                })
+                            })
+
+                    },
+                    err => {
+                        wx.showToast({
+                            icon: 'none',
+                            title: err.message,
+                        })
+                    }
+                )
+            },
+            fail: err => {
+                console.log(err);
+            }
+        })
+    },
+
+    // 退出登录
+    confirmExit: function () {
+        wx.clearStorage({
+            success: (res) => {
+                console.log(res)
+                wx.showToast({
+                    icon: 'success',
+                    title: '退出登录成功',
+                    duration: 2000,
+                })
+
+                setTimeout(() =>
+                    // app.reloadThisPage(),
+                    wx.redirectTo({
+                        url: '/pages/index/login/login',
+                    }), 1000
+                )
+            },
         })
     },
 
--- utils/httputils.js
+++ utils/httputils.js
@@ -11,44 +11,44 @@ const app = getApp()
 const baseURL = 'http://localhost:10077/wehgu'
 
 function fill_token_toheader(token) {
-  if (token == null || token == undefined || token == "") {
-    token = wx.getStorageSync('unionId');
-  }
-  header["unionId"] = token;
-  // console.log(token)
-  return (token)
+    if (token == null || token == undefined || token == "") {
+        token = wx.getStorageSync('unionId');
+    }
+    header["unionId"] = token;
+    // console.log(token)
+    return (token)
 }
 const header = {
-  'os': 'android',
-  'version': '1.0.0',
-  'sessionId': wx.getStorageSync('sessionId'),
-  'unionId': wx.getStorageSync('unionId'),
-  'AuthorizationAppWx': 'wxeb4f620b577ff31a',
-  'openId': wx.getStorageSync('openId')
+    'os': 'android',
+    'version': '1.0.0',
+    'sessionId': wx.getStorageSync('sessionId'),
+    'unionId': wx.getStorageSync('unionId'),
+    'AuthorizationAppWx': 'wxeb4f620b577ff31a',
+    'openId': wx.getStorageSync('openId')
 }
 /**
  * 供外部post请求调用  
  */
 function post(url, params, contenttypeEnum, onSuccess, onFailed) {
-  // console.log("请求方式：", "POST")
-  if (contenttypeEnum == ContentTypeEnum.Form_Sub) {
-    header["content-type"] = "application/x-www-form-urlencoded";
-  } else if (contenttypeEnum == ContentTypeEnum.Json_Sub) {
-    header["content-type"] = "application/json;charset=UTF-8";
-  } else if (contenttypeEnum == ContentTypeEnum.Upload_Sub) {
-    header["content-type"] = "multipart/form-data";
-  } else {
-    header["content-type"] = "application/json;charset=UTF-8";
-  }
-  request(url, params, "POST", onSuccess, onFailed);
+    // console.log("请求方式：", "POST")
+    if (contenttypeEnum == ContentTypeEnum.Form_Sub) {
+        header["content-type"] = "application/x-www-form-urlencoded";
+    } else if (contenttypeEnum == ContentTypeEnum.Json_Sub) {
+        header["content-type"] = "application/json;charset=UTF-8";
+    } else if (contenttypeEnum == ContentTypeEnum.Upload_Sub) {
+        header["content-type"] = "multipart/form-data";
+    } else {
+        header["content-type"] = "application/json;charset=UTF-8";
+    }
+    request(url, params, "POST", onSuccess, onFailed);
 }
 
 /**
  * 供外部get请求调用
  */
 function get(url, params, onSuccess, onFailed) {
-  // console.log("请求方式：", "GET")
-  request(url, params, "GET", onSuccess, onFailed);
+    // console.log("请求方式：", "GET")
+    request(url, params, "GET", onSuccess, onFailed);
 }
 
 /**
@@ -61,54 +61,65 @@ function get(url, params, onSuccess, onFailed) {
  */
 
 function request(url, params, method, onSuccess, onFailed) {
-//   wx.showLoading({
-//     title: "正在加载中...",
-//   })
+    //   wx.showLoading({
+    //     title: "正在加载中...",
+    //   })
 
-  let fullpath = getFullRequestPath(url);
-  header["Cookie"] = wx.getStorageSync('cookie');
-  header["sessionId"] = wx.getStorageSync('sessionId');
-  header["openid"] = wx.getStorageSync('openid');
-  wx.request({
-    url: fullpath,
-    data: dealParams(params),
-    method: method,
-    header: header,
-    success: function (res) {
-    //   wx.hideLoading()
-      // console.log('响应：', res);
-      if (res.header) {
-        if ('Set-Cookie' in res.header) {
-          if (res.header['Set-Cookie']) {
-            wx.setStorageSync('cookie', res.header['Set-Cookie']);
-          }
-        } else if ('set-cookie' in res.header) {
-          if (res.header['set-cookie']) {
-            wx.setStorageSync('cookie', res.header['Set-Cookie']);
-          }
+    let fullpath = getFullRequestPath(url);
+    header["Cookie"] = wx.getStorageSync('cookie');
+    header["sessionId"] = wx.getStorageSync('sessionId');
+    header["openid"] = wx.getStorageSync('openid');
+    wx.request({
+        url: fullpath,
+        data: dealParams(params),
+        method: method,
+        header: header,
+        success: function (res) {
+            //   wx.hideLoading()
+            // console.log('响应：', res);
+            if (res.header) {
+                if ('Set-Cookie' in res.header) {
+                    if (res.header['Set-Cookie']) {
+                        wx.setStorageSync('cookie', res.header['Set-Cookie']);
+                    }
+                } else if ('set-cookie' in res.header) {
+                    if (res.header['set-cookie']) {
+                        wx.setStorageSync('cookie', res.header['Set-Cookie']);
+                    }
+                }
+            }
+            if (res.data.code === 200) {
+                onSuccess(res.data); //request success
+            } else if (res.data.code === 403 || res.data.code === 401) {
+                wx.showModal({
+                    content: '登录已过期,请重新登录！',
+                    success: res => {
+                        if (res.confirm) {
+                            app.getUserProfile()
+                        } else if (res.cancel) {
+                            app.confirmExit()
+                        }
+                    }
+                })
+            } else {
+                wx.showToast({
+                    title: res.data.message,
+                    icon: 'none',
+                    duration: 2000
+                })
+                onFailed(res.data)
+            }
+        },
+        fail: function (error) {
+            //   wx.hideLoading();
+            wx.showToast({
+                title: 'Network Error',
+                icon: 'none',
+                duration: 2000
+            })
+            onFailed(error); //failure for other reasons
         }
-      }
-      if (res.data.code === 200) {
-        onSuccess(res.data); //request success
-      } else {
-        wx.showToast({
-          title: res.data.message,
-          icon: 'none',
-          duration: 2000
-        })
-        onFailed(res.data)
-      }
-    },
-    fail: function (error) {
-    //   wx.hideLoading();
-      wx.showToast({
-        title: 'Network Error',
-        icon: 'none',
-        duration: 2000
-      })
-      onFailed(error); //failure for other reasons
-    }
-  })
+    })
 }
 
 /**
@@ -116,20 +127,20 @@ function request(url, params, method, onSuccess, onFailed) {
  * @params 请求参数
  */
 function dealParams(params) {
-  // console.log("请求参数:", params)
-  return params;
+    // console.log("请求参数:", params)
+    return params;
 }
 
 function getFullRequestPath(url) {
-  return baseURL + url;
+    return baseURL + url;
 }
 
 
 // 1.通过module.exports方式提供给外部调用
 module.exports = {
-  baseURL: baseURL,
-  postRequest: post,
-  getRequest: get,
-  fill_token_toheader: fill_token_toheader,
-  getFullRequestPath: getFullRequestPath
+    baseURL: baseURL,
+    postRequest: post,
+    getRequest: get,
+    fill_token_toheader: fill_token_toheader,
+    getFullRequestPath: getFullRequestPath
 }
