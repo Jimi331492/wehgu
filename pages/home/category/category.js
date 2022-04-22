22a3f0a948672d9fe92af3417fdf92f19d182815
Jimi331492 <jimi331492@gmail.com>
2022-04-22 09:51:45 +0800

    新增页面

--- pages/home/category/category.js
+++ pages/home/category/category.js
@@ -8,18 +8,71 @@ Page({
      * 页面的初始数据
      */
     data: {
+        bakPath: '/images/custom-avatar.png',
         title: null,
+        tag: null,
         query: {
             limit: 6,
             page: 1,
             auditStatus: '正常',
             tag: null,
+            content: null,
         },
+        content: null,
         total: 0,
         postList: [],
+        starList: [],
 
         triggered: false,
         isBottom: false,
+    },
+    contentInput(e) {
+        this.setData({
+            content: e.detail.value
+        })
+    },
+    async reset() {
+        const query = {
+            limit: 6,
+            page: 1,
+            auditStatus: '正常',
+            tag: this.data.tag,
+            content: null,
+        };
+        this.setData({
+            content: null,
+            isBottom: false,
+            total: 0,
+            query: query
+        })
+        const res = await this.getPostList(this.data.query)
+        this.data.total = res.data.total
+        this.setData({
+            postList: res.data.records
+        })
+    },
+    async searchPost() {
+        if (this.data.content === null || this.data.content === '') {
+            wx.showToast({
+                icon: 'none',
+                title: '请输入关键字查询',
+            })
+            return false
+        }
+        //重置
+        this.data.query = {
+            limit: 6,
+            page: 1,
+            auditStatus: '正常',
+            tag: this.data.tag,
+            content: this.data.content,
+        };
+        const res = await this.getPostList(this.data.query)
+        this.data.total = res.data.total
+
+        this.setData({
+            postList: res.data.records
+        })
     },
 
     onPulling(e) {},
@@ -32,13 +85,14 @@ Page({
         this.data.query = {
             limit: 6,
             page: 1,
-            auditStatus: '正常'
+            auditStatus: '正常',
+            tag: this.data.tag
         };
         this.data.postList = [];
         this.data.total = 0;
         const res = await this.getPostList(this.data.query);
         this.setData({
-            postList: res.data
+            postList: res.data.records
         })
         setTimeout(() => {
             this.setData({
@@ -61,7 +115,9 @@ Page({
         console.log(options);
         this.setData({
             ['query.tag']: options.tag,
-            title: options.name
+            title: options.name,
+            tag: options.tag,
+            starList: app.globalData.starList,
         })
         const res = await this.getPostList(this.data.query)
         this.data.total = res.data.total
@@ -80,6 +136,11 @@ Page({
             http.postRequest('/post/getPostPage', query, ContentTypeEnum.Json_Sub,
                 res => {
                     resolve(res)
+                    if (res.data.total <= 6) {
+                        this.setData({
+                            isBottom: true,
+                        })
+                    }
                     wx.hideLoading();
                 }, err => {
                     console.log(err);
@@ -87,6 +148,35 @@ Page({
                     wx.hideLoading();
                 })
         })
+    },
+
+    async loadPostList() {
+        if (this.data.query.limit * this.data.query.page >= this.data.total) {
+            if (this.data.isBottom) {
+                wx.showToast({
+                    icon: "none",
+                    title: '到底了',
+                })
+            }
+            this.setData({
+                isBottom: true
+            })
+        }
+        if (!this.data.isBottom) {
+            this.data.query.page += 1;
+            const res = await this.getPostList(this.data.query)
+            let postList = [];
+            this.data.total = res.data.total
+            if (this.data.postList.length < 1) {
+                postList = res.data.records
+            } else {
+                postList = this.data.postList.concat(res.data.records)
+            }
+            this.setData({
+                postList: postList
+            })
+        }
+
     },
 
     /**
